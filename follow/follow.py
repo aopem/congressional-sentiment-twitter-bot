2b9798d1eeb785f9cdc4fc3bad16f5c247d760a0
Andrew Opem <andrewopem@gmail.com>
2022-08-09 15:17:31 -0700

    format for azure functions

--- follow/follow.py
+++ follow/follow.py
@@ -1,5 +1,6 @@
 import json
 import azure.functions as func
+import logging
 
 from twitter_bot.client.twitter import BotClient
 from twitter_bot.model import TwitterUser
@@ -7,7 +8,9 @@ import twitter_bot.utils.constants as c
 import twitter_bot.utils.functions as f
 
 
-def run():
+def run(
+    in_found: str
+):
     # create bot client
     secrets = f.get_secrets_dict()
     bot = BotClient(
@@ -18,29 +21,40 @@ def run():
         bearer_token=secrets["bearerToken"]
     )
 
+    # create set of users already following
+    logging.info("Retrieving currently followed users from Twitter...")
+    following = bot.getFollowing()
+    following_ids = {}
+    for user in following:
+        following_ids.add(user.id)
+
     # read json containing twitter account info
-    twitter_user_json = json.load(open(c.TWITTER_ACCOUNTS_FOUND_FILENAME))
+    logging.info("Loading twitter accounts to follow from getusers/found.json...")
+    twitter_user_json = json.loads(in_found)
 
     # follow all users - no errors if already following
     for user_json in twitter_user_json:
-        user = TwitterUser(
-            id=user_json["id"],
-            name=user_json["name"],
-            username=user_json["username"],
-            verified=user_json["verified"]
-        )
-        try:
-            bot.followUser(user)
-            print(f"Successfully followed @{user.username} ({user.name})")
-        except Exception:
-            print(f"WARN: Could not follow user @{user.username} ({user.name})")
+        if user_json["id"] not in following_ids:
+            user = TwitterUser(
+                id=user_json["id"],
+                name=user_json["name"],
+                username=user_json["username"],
+                verified=user_json["verified"]
+            )
+
+            try:
+                bot.followUser(user)
+                logging.info(f"Successfully followed @{user.username} ({user.name})")
+            except Exception:
+                logging.warn(f"Could not follow user @{user.username} ({user.name})")
 
     return
 
 
-def main(timer: func.TimerRequest):
-    if timer.past_due:
-        run()
+def main(
+    inFound: str,
+):
+    run(inFound)
 
 
 if __name__ == "__main__":
