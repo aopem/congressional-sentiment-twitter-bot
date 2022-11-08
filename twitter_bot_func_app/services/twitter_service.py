59ed2c6a1571f621edea9614b802da00432c1419
Andrew Opem <andrewopem@gmail.com>
2022-11-07 21:09:02 -0800

    formatting

--- twitter_bot_func_app/services/twitter_service.py
+++ twitter_bot_func_app/services/twitter_service.py
@@ -12,9 +12,9 @@ class TwitterService:
     ) -> str:
         text = f"Based on the {TWITTER_MAX_TWEETS_RETURNED} most recent mentions " \
                f"of #uscongress member @{user.username}, I have given them "       \
-               f"a {sentiment_analysis.category} Twitter sentiment score of "         \
-               f"{sentiment_analysis.value:.3f} (-1 to +1). I found "                 \
-               f"{sentiment_analysis.num_positive} positive tweet(s) and "            \
+               f"a {sentiment_analysis.category} Twitter sentiment score of "      \
+               f"{sentiment_analysis.value:.3f} (-1 to +1). I found "              \
+               f"{sentiment_analysis.num_positive} positive tweet(s) and "         \
                f"{sentiment_analysis.num_negative} negative tweet(s)"
 
         return text
